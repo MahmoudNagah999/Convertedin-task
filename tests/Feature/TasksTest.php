32b9e8f1c5f9c44e84393d51b88f8339148c2dd9
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2024-08-17 17:19:43 +0300

    test

---
+++ tests/Feature/TasksTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TasksTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+        $response->assertSee('Tasks');
+        $response->assertStatus(200);
+    }
+}
